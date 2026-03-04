a9b98296919d559fd5e9aed40c00ba9455993f1c
karthik <pasulakarthik03@gmail.com>
2026-03-04 19:09:28 +0530

    fixed jose package issue

--- database.py
+++ database.py
@@ -1,39 +1,21 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.ext.declarative import declarative_base
-# import os
-
-# db_url = os.getenv("DATABASE_URL")
-
-
-# engine = create_engine(db_url)
-
-# SessionLocal = sessionmaker(engine , autocommit = False, autoflush=False,)
-
-# Base = declarative_base()
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-db_url = "postgresql+psycopg2://fastapi_user:karthik8267@localhost:5432/fastapi_db"
+db_url = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(db_url)
+
 SessionLocal = sessionmaker(engine , autocommit = False, autoflush=False,)
 
 Base = declarative_base()
 
 def get_db():
-    
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
