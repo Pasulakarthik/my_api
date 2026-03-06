1a74cbc2d85cc42822ce7c9d64ac6f1de0d19a5d
Pasulakarthik <pasulakarthik03@gmail.com>
2026-03-06 19:22:56 +0530

    add category

--- model.py
+++ model.py
@@ -18,6 +18,7 @@ class Product(Base):
     price = Column(Integer, nullable=False)
     brand = Column(String, nullable=False)
     stock = Column(Integer , nullable=False)
+    category = Column(String,nullable=False)
 
 
 class Cart(Base):
